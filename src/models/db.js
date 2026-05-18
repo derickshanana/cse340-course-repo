2c14dadc72303ac4b8f88005986ab0f14764e4b6
derickshanana <derickshanana50@gmail.com>
2026-05-18 18:59:37 +0200

    ssl fix

--- src/models/db.js
+++ src/models/db.js
@@ -12,9 +12,12 @@ import { Pool } from 'pg';
  * The connection string format is:
  * postgresql://username:password@host:port/database
  */
+// After
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 /**
